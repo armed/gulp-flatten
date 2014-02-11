d03c66e0e39a5f5e7df2d68dff51662d740e73bb
Dmitry Lutsik <otoyto@gmail.com>
2014-02-11 11:10:07 +0200

    process files only

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var fs = require('fs');
 var through2 = require('through2');
 var PluginError = require('gulp-util').PluginError;
 
@@ -7,12 +8,17 @@ module.exports = function(opts) {
   opts.newPath = opts.newPath || '';
 
   return through2.obj(function(file, enc, next) {
-    try {
-      file.path = path.join(file.base, opts.newPath, path.basename(file.path));
-      this.push(file);
-    } catch (e) {
-      this.emit('error', new PluginError('gulp-flatten', e));
-    }
-    next();
+    var that = this;
+
+    fs.lstat(file.path, function (err, stats) {
+      if (!stats.isFile()) return next();
+      try {
+          file.path = path.join(file.base, opts.newPath, path.basename(file.path));
+          that.push(file);
+      } catch (e) {
+        that.emit('error', new PluginError('gulp-flatten', e));
+      }
+      next();
+    });
   });
 };
