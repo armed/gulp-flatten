6943c0e943f8d28883127360c760b289c9573a1b
Saulo Vallory <me@saulovallory.com>
2014-03-16 13:09:23 -0300

    Adds keepTopDir option

--- index.js
+++ index.js
@@ -8,7 +8,18 @@ module.exports = function(opts) {
 
   return through2.obj(function(file, enc, next) {
     try {
-      file.path = path.join(file.base, opts.newPath, path.basename(file.path));
+      var topDir = "";
+
+      if(opts.keepTopDir)
+      {
+        var relPath = path.relative(file.base, file.path);
+        topDir = relPath.split(/[\/\\\\]/)[0];
+        
+        if(topDir == path.basename(file.path))
+          topDir = "";
+      }
+
+      file.path = path.join(file.base, opts.newPath, topDir, path.basename(file.path));
       this.push(file);
     } catch (e) {
       this.emit('error', new PluginError('gulp-flatten', e));
